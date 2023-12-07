122b5e0b52ba785cfce790bd4065c68cfee1cda8
YeongJe40 <145317678+YeongJe40@users.noreply.github.com>
2023-12-07 13:26:57 +0900

    Title: 2가 3번째로 등장하는 위치, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
+++ 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int i, n, h,cnt=0;
-    scanf("%d",&n);
-    for(i=0;i<n+1;i++)
-    {
-        scanf("%d ",&h);
-        if(h==2)
-        {
-            cnt+=1;
-            if (cnt==3)
-            {
-                printf(i);
-            }
-        }
-        
-            
-    }
+    // 정수 n과 배열을 입력받습니다.
+	int n;
+	scanf("%d", &n);
+	int arr[100];
+	for(int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	
+	// 2가 나온 횟수를 카운트해 줍니다.
+	int cnt = 0;
+	for(int i = 0; i < n; i++) {
+		if(arr[i] == 2)
+			cnt++;
+		
+		// 2가 3번째로 등장할 때 그 위치를 출력합니다.
+		if(cnt == 3) {
+			printf("%d", i + 1);
+			break;
+		}
+	}
+	
     return 0;
+}
 }
